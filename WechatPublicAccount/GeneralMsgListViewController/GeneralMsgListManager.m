926f99cafeb2a7694bf2b504589b6a87dbe3b7a0
mayan <may-g@glodon.com>
2020-07-15 10:39:41 +0800

    优化 GeneralMsg 列表页面，取消网络请求

--- WechatPublicAccount/GeneralMsgListViewController/GeneralMsgListManager.m
+++ WechatPublicAccount/GeneralMsgListViewController/GeneralMsgListManager.m
@@ -29,40 +29,33 @@
 
 - (void)fetchGeneralMsgListWithId:(NSString *)accountId isFromNetwork:(BOOL)isFromNetwork completed:(void (^)(NSArray<GeneralMsg *> * _Nonnull, NSError * __nullable))completedBlock {
     if (isFromNetwork) {
-        [self downloadZipWithId:accountId completed:^(NSURLResponse * _Nonnull response, NSURL * _Nonnull filePath, NSError * _Nonnull error) {
+        NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+        NSString *zipPath = [[NSBundle mainBundle] pathForResource:accountId ofType:@"zip"];
+        NSString *unzipPath = [documentsPath stringByAppendingPathComponent:accountId];
+        
+        [SSZipArchive unzipFileAtPath:zipPath toDestination:documentsPath progressHandler:nil completionHandler:^(NSString * _Nonnull path, BOOL succeeded, NSError * _Nullable error) {
             
             if (error && completedBlock) {
               completedBlock(@[], error);
             }
             
-            NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
-            NSString *zipPath = [documentsPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.zip", accountId]];
-            NSString *unzipPath = [documentsPath stringByAppendingPathComponent:accountId];
+            NSArray *dir = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:unzipPath error:nil];
             
-            [SSZipArchive unzipFileAtPath:zipPath toDestination:documentsPath progressHandler:nil completionHandler:^(NSString * _Nonnull path, BOOL succeeded, NSError * _Nullable error) {
-                
-                if (error && completedBlock) {
-                  completedBlock(@[], error);
+            NSMutableArray *responseObjects = [NSMutableArray array];
+            for (NSString *path in dir) {
+                NSData *data = [NSData dataWithContentsOfFile:[unzipPath stringByAppendingPathComponent:path]];
+                NSDictionary *responseObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+                if (responseObject) {
+                    data = [responseObject[@"general_msg_list"] dataUsingEncoding:NSUTF8StringEncoding];
+                    responseObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
+                    [responseObjects addObjectsFromArray:responseObject[@"list"]];
                 }
-                
-                NSArray *dir = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:unzipPath error:nil];
-                
-                NSMutableArray *responseObjects = [NSMutableArray array];
-                for (NSString *path in dir) {
-                    NSData *data = [NSData dataWithContentsOfFile:[unzipPath stringByAppendingPathComponent:path]];
-                    NSDictionary *responseObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
-                    if (responseObject) {
-                        data = [responseObject[@"general_msg_list"] dataUsingEncoding:NSUTF8StringEncoding];
-                        responseObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:nil];
-                        [responseObjects addObjectsFromArray:responseObject[@"list"]];
-                    }
+            }
+            
+            [[CoreDataManager shareInstance] updateGeneralMsgsWithDataArray:responseObjects accountId:accountId completed:^(BOOL contextDidSave, NSError * _Nullable error) {
+                if (completedBlock) {
+                    completedBlock([[CoreDataManager shareInstance] generalMsgsWithAccountId:accountId], error);
                 }
-                
-                [[CoreDataManager shareInstance] updateGeneralMsgsWithDataArray:responseObjects accountId:accountId completed:^(BOOL contextDidSave, NSError * _Nullable error) {
-                    if (completedBlock) {
-                        completedBlock([[CoreDataManager shareInstance] generalMsgsWithAccountId:accountId], error);
-                    }
-                }];
             }];
         }];
     } else {
@@ -78,28 +71,6 @@
             completedBlock(newGeneralMsg);
         }
     }];
-}
-
-
-#pragma mark - Pravite Methods
-
-// 下载压缩包，并保存到 Documents 中
-- (void)downloadZipWithId:(NSString *)accountId completed:(nonnull void (^)(NSURLResponse * _Nonnull, NSURL * _Nonnull, NSError * _Nonnull))completedBlock {
-    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-
-    NSURL *URL = [NSURL URLWithString:[NSString stringWithFormat:@"https://mayan29.oss-cn-beijing.aliyuncs.com/wechat-public-account/zip/%@.zip", accountId]];
-    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
-
-    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
-        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
-        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
-    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
-        if (completedBlock) {
-            completedBlock(response, filePath, error);
-        }
-    }];
-    [downloadTask resume];
 }
 
 
